956603d210c9cc3e35b8f10abc8bcdc1ccdff0d6
rowanhere <uzumakigaito@gmail.com>
2024-04-10 22:41:25 +0545

    oreBug

--- checkOre.js
+++ checkOre.js
@@ -1,32 +1,43 @@
-import { spawnSync  } from "child_process";
+import { spawnSync } from "child_process";
 import fs from "fs";
 
 const PATH = "/root/.config/solana/";
 
 const getRewardsTable = async () => {
   try {
- 
     const ListOfKeys = fs
       .readdirSync(PATH, { withFileTypes: true })
       .filter((item) => item.isFile() && item.name.endsWith(".json"))
       .map((item) => `${item.path}/${item.name}`);
-     console.log(ListOfKeys);
-    if (!(ListOfKeys.length > 0))
+    console.log(ListOfKeys);
+
+    if (ListOfKeys.length === 0)
       throw new Error("No private keys found. Recheck your keys path.");
 
     const getData = ListOfKeys.map((keys) => {
       return new Promise((resolve, reject) => {
         const getAddress = spawnSync("solana", ["address", "-k", keys]);
         const getBalance = spawnSync("ore", ["--keypair", keys, "rewards"]);
-      
-       resolve(
-        {
-            Address:getAddress.error?"Solana Cli not found":getAddress.stdout?.toString().trim() || "Invalid",
-            Balance :getBalance.error?"Ore Cli not found": getBalance.stdout?.toString().trim() || "0"
+
+        // Wait for spawnSync calls to complete
+        if (getAddress.error || getBalance.error) {
+          resolve({
+            Address: getAddress.error
+              ? "Solana Cli not found"
+              : getAddress.stdout?.toString().trim() || "Invalid",
+            Balance: getBalance.error
+              ? "Ore Cli not found"
+              : getBalance.stdout?.toString().trim() || "0",
+          });
+        } else {
+          resolve({
+            Address: getAddress.stdout?.toString().trim() || "Invalid",
+            Balance: getBalance.stdout?.toString().trim() || "0",
+          });
         }
-       )
       });
     });
+
     const signatures = await Promise.all(getData);
     console.table(signatures);
   } catch (err) {
