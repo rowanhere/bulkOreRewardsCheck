5f4b570a489467ea35e02543cf8f3fdbe7b3bb77
rowanhere <uzumakigaito@gmail.com>
2024-04-10 22:26:28 +0545

    first commit

---
+++ checkOre.js
@@ -0,0 +1,36 @@
+import { spawnSync  } from "child_process";
+import fs from "fs";
+
+const PATH = "./keys";
+
+const getRewardsTable = async () => {
+  try {
+    const ListOfKeys = fs
+      .readdirSync(PATH, { withFileTypes: true })
+      .filter((item) => item.isFile() && item.name.endsWith(".json"))
+      .map((item) => `${item.path}/${item.name}`);
+
+    if (!(ListOfKeys.length > 0))
+      throw new Error("No private keys found. Recheck your keys path.");
+
+    const getData = ListOfKeys.map((keys) => {
+      return new Promise((resolve, reject) => {
+        const getAddress = spawnSync("solana", ["address", "-k", keys]);
+        const getBalance = spawnSync("ore", ["--keypair", keys, "rewards"]);
+      
+       resolve(
+        {
+            Address:getAddress.error?"Solana Cli not found":getAddress.stdout?.toString().trim() || "Invalid",
+            Balance :getBalance.error?"Ore Cli not found": getBalance.stdout?.toString().trim() || "0"
+        }
+       )
+      });
+    });
+    const signatures = await Promise.all(getData);
+    console.table(signatures);
+  } catch (err) {
+    console.log("Error", err);
+  }
+};
+
+getRewardsTable();
---
+++ readme.md
@@ -0,0 +1,26 @@
+# Ore Rewards Checker
+
+## Introduction
+
+This program is designed to retrieve and display reward balances for Solana addresses using private keys stored in a specified directory. It utilizes the Solana CLI tool and the "ore" command-line tool to fetch address information and rewards balances respectively.
+
+## Requirements
+
+- Node.js installed on your system
+- Solana CLI tool installed
+- Ore cli tool installed
+- Access to a Solana wallet with stored private keys
+
+## Installation
+
+1. Clone this repository to your local machine.
+2. Navigate to the project directory.
+3. Install dependencies using npm:
+
+```bash
+npm install
+```
+
+## Run Program
+```
+node checkOre
